ed0f74c83a7835bdbcf6c044ce4ee5231efe7ec0
Myles “Dylesmavis” Whitcher <myleswhitcher@gmail.com>
2026-08-23 17:12:52 -0700

    feat: install mobile runtime at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
+import { installMobileRuntime } from "./engine/mobileRuntime";
 import "./index.css";
+
+installMobileRuntime();
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
